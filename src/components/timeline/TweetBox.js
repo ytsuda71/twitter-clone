90e56909549c001f3ef9730aaca3fe1141fe1689
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2022-06-02 12:15:08 +0900

    apply TweetBox.css

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,12 +1,15 @@
 import { Avatar, Button } from '@mui/material'
 import React from 'react'
+import './TweetBox.css'
 
 function TweetBox() {
   return (
     <div className='tweetBox'>
         <form>
-            <Avatar />
-            <input placeholder="What's happening?" type='text'></input>
+            <div className='tweetBox--input'>
+                <Avatar />
+                <input placeholder="What's happening?" type='text'></input>
+            </div>
             <input className='tweetBox--imageInput' placeholder='Optional: Enter image URL' type='text'></input>
             <Button className='tweetBox--tweetButton' type='submit'>Tweet</Button>
         </form>
