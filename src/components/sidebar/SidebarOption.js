2450e6e0efd5bdd0c6642c00bcb4eb4c4723ab67
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2022-06-02 10:02:00 +0900

    apply SidebarOption.css

--- src/components/sidebar/SidebarOption.js
+++ src/components/sidebar/SidebarOption.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './SidebarOption.css'
 
 function SidebarOption({text, Icon}) {
   return (
-    <div>
+    <div className='sidebarOption'>
         <Icon/>
         <h2>{text}</h2>
     </div>
