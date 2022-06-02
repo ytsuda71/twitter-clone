b0217bee4c8d454db2ed5c65239cb3a2c6172a43
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2022-06-02 10:53:13 +0900

    create timeline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import './components/sidebar/Sidebar'
 import Sidebar from './components/sidebar/Sidebar';
+import Timeline from './components/timeline/Timeline';
 
 function App() {
   return (
     <div className="app">
+      {/* Sidebar */}
       <Sidebar/>
+
+      {/* Timeline */}
+      <Timeline/>
+      {/* Widget */}
+
     </div>
   );
 }
---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+        {/* Header */}
+        <div className='timeline_header'>
+            <h2>Home</h2>
+        </div>
+        {/* TweetBox */}
+        {/* Post */}
+    </div>
+  )
+}
+
+export default Timeline
