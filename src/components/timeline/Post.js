e9c6cf95b4b500d314a458dc90c2dbc1774711af
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2022-06-02 15:36:55 +0900

    create Post

---
+++ src/components/timeline/Post.js
@@ -0,0 +1,36 @@
+import { ChatBubbleOutline, FavoriteBorder, PublishOutlined, Repeat, VerifiedUser } from '@mui/icons-material'
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+function Post() {
+  return (
+    <div className='post'>
+        <div className='post--avatar'>
+            <Avatar/>
+        </div>
+        <div className='post--body'>
+            <div className='post--header'>
+                <div className='post--headerText'>
+                    <h3>Test</h3>
+                    <span className='post--headerSpecial'>
+                        <VerifiedUser className='post-badge' />
+                        @sarusiya
+                    </span>
+                </div>
+                <div className='post-headerDescription'>
+                    <p>Test now</p>
+                </div>
+            </div>
+            <img src="https://source.unsplash.com/random" />
+            <div className='post-footer'>
+                <ChatBubbleOutline fontSize='small'/>
+                <Repeat fontSize='small' />
+                <FavoriteBorder fontSize='small' />
+                <PublishOutlined fontSize='small' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Post
--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Post from './Post'
 import './Timeline.css'
 import TweetBox from './TweetBox'
 
@@ -12,6 +13,7 @@ function Timeline() {
         {/* TweetBox */}
         <TweetBox/>
         {/* Post */}
+        <Post />
     </div>
   )
 }
