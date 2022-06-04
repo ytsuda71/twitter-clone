9b9063ee85ec58eabd199e42ef26f62fafdbc8e0
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2022-06-04 22:24:49 +0900

    improve TweetBox.js so that tweet can be posted

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,20 +1,56 @@
-import { Avatar, Button } from '@mui/material'
-import React from 'react'
-import './TweetBox.css'
+import { Avatar, Button } from "@mui/material";
+import React, { useState } from "react";
+import "./TweetBox.css";
+import { collection, addDoc } from "firebase/firestore";
+import db from "../../firebase";
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+  const sendTweet = (e) => {
+    /* firebaseのデータベースにデータを追加する */
+    e.preventDefault();
+    addDoc(collection(db, "posts"), {
+      displayName: "displayName",
+      username: "userName",
+      verified: true,
+      tweet: tweetMessage,
+      avatar: "https://joeschmoe.io/api/v1/random",
+      image: tweetImage,
+    });
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
   return (
-    <div className='tweetBox'>
-        <form>
-            <div className='tweetBox--input'>
-                <Avatar />
-                <input placeholder="What's happening?" type='text'></input>
-            </div>
-            <input className='tweetBox--imageInput' placeholder='Optional: Enter image URL' type='text'></input>
-            <Button className='tweetBox--tweetButton' type='submit'>Tweet</Button>
-        </form>
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox--input">
+          <Avatar />
+          <input
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+            onChange={(e) => setTweetMessage(e.target.value)}
+          ></input>
+        </div>
+        <input
+          value={tweetImage}
+          className="tweetBox--imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+          onChange={(e) => setTweetImage(e.target.value)}
+        ></input>
+        <Button
+          className="tweetBox--tweetButton"
+          type="submit"
+          onClick={sendTweet}
+        >
+          Tweet
+        </Button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default TweetBox
+export default TweetBox;
